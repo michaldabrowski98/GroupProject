3e4a6bae18f6c1fbca263fc2d63032d17832ef1e
Osio <oskar.rychtelski@gmail.com>
2024-01-26 21:49:55 +0100

    small fixes

--- frontend/app/src/components/Footer.vue
+++ frontend/app/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <v-footer class="d-flex flex-column" fixed padless height="64px">
-    <div class="tilebar">2023 &copy; QuizApp</div>
+    <div class="tilebar">2024 &copy; QuizApp</div>
     <div style="clear: both;"></div>
   </v-footer>
 </template>
